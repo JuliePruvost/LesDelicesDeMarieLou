b8ea611f63b66c6b26520bb9ae902290dd35a2c6
JuliePruvost <julie.de.pruvost@gmail.com>
2022-10-06 17:51:33 +0200

    Update Map.tsx

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -1,41 +1,4 @@
 import React from "react";
-import { createCustomEqual } from "fast-equals";
-import { isLatLngLiteral } from "@googlemaps/typescript-guards";
-
-const deepCompareEqualsForMaps = createCustomEqual(
-    (deepEqual) => (a: any, b: any) => {
-        if (
-            isLatLngLiteral(a) ||
-            a instanceof google.maps.LatLng ||
-            isLatLngLiteral(b) ||
-            b instanceof google.maps.LatLng
-        ) {
-            return new google.maps.LatLng(a).equals(new google.maps.LatLng(b));
-        }
-
-        // TODO extend to other types
-
-        // use fast-equals for other objects
-        return deepEqual(a, b);
-    }
-);
-
-function useDeepCompareMemoize(value: any) {
-    const ref = React.useRef();
-
-    if (!deepCompareEqualsForMaps(value, ref.current)) {
-        ref.current = value;
-    }
-
-    return ref.current;
-}
-
-function useDeepCompareEffectForMaps(
-    callback: React.EffectCallback,
-    dependencies: any[]
-) {
-    React.useEffect(callback, dependencies.map(useDeepCompareMemoize));
-}
 
 interface MapProps extends google.maps.MapOptions {
     style: { [key: string]: string };
@@ -50,25 +13,17 @@ export default function Map({style, children, ...options}: MapProps) {
 
     React.useEffect(() => {
     if (ref.current && !map) {
-        setMap(new window.google.maps.Map(ref.current, {}));
+        setMap(new window.google.maps.Map(ref.current, options));
     }
     }, [ref, map]);
-
-    // because React does not do deep comparisons, a custom hook is used
-    // see discussion in https://github.com/googlemaps/js-samples/issues/946
-    useDeepCompareEffectForMaps(() => {
-        if (map) {
-        map.setOptions(options);
-        }
-    }, [map, options]);
 
     return (
         <>
             <div ref={ref} style={style} />
             {React.Children.map(children, (child) => {
                 if (React.isValidElement(child)) {
-                // set the map prop on the child component
-                return React.cloneElement(child, { map });
+                    // set the map prop on the child component
+                    return React.cloneElement(child, { map } as any);
                 }
             })}
         </>
