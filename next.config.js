21e4fb460f3d17fd26350592b333e441eec93e76
JuliePruvost <julie.de.pruvost@gmail.com>
2022-10-06 18:32:38 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,17 @@
 
 const path = require('path');
 
-const repo = 'LesDelicesDeMarieLou'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
 const nextConfig = {
     reactStrictMode: true,
