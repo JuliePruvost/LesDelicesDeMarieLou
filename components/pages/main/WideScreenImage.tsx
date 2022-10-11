e772527090cfed7c1e5eb673a6bbf8865ab29238
JuliePruvost <julie.de.pruvost@gmail.com>
2022-10-11 09:12:38 +0200

    fix main image height

--- components/pages/main/WideScreenImage.tsx
+++ components/pages/main/WideScreenImage.tsx
@@ -1,19 +1,42 @@
 import Image from "next/image";
-import boutique_large from '/public/boutique_large.jpeg'
-import styles from './WideScreenImage.module.scss';
+import boutique_large from "/public/boutique_large.jpeg";
+import styles from "./WideScreenImage.module.scss";
+import { useEffect, useState } from "react";
 
 export default function WideScreenImage() {
+    const [width, setWidth] = useState<number>(0);
+
+    useEffect(() => {
+        function handleResize() {
+            setWidth(window.innerWidth);
+        }
+
+        window.addEventListener("resize", handleResize);
+
+        handleResize();
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    });
+
+    const height = width < 480 ? '80vh' 
+        : width < 600 ? '70vh'
+        : width < 800 ? '60vh'
+        : width < 1000 ? '50vh'
+        : '30vh'
     return (
-        <div style={{display: 'block'}}>
+        <div style={{ display: "block" }}>
             <Image
                 src={boutique_large}
                 alt="accueil les delices de marie lou"
-                height="30vh"
+                height={height}
                 width="100vw"
                 objectFit="cover"
                 objectPosition="50% 30%"
                 layout="responsive"
-                className={styles.wideScreenImage}            />
+                className={styles.wideScreenImage}
+            />
         </div>
     );
 }
